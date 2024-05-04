30669b6afc5a926aaf5185424672ecdac515ace3
Simon Wisdom <simonowisdom@gmail.com>
2024-05-03 19:28:22 -0600

    updated to ES syntax

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('dist'));  // Ensure your build output folder is 'dist'
+app.use(express.static(join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
